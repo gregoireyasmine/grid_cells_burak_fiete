a2bff465b172b22e1ba8e1df5bfd1a6fc6bd6dab
gregoireyasmine <gregoire.degobert-yasmine@ens.psl.eu>
2025-01-02 14:01:36 +0000

    Fixed RNN

--- plotting.py
+++ plotting.py
@@ -25,7 +25,7 @@ def plot_10_frames(traj, dt, plot_center = True, plot_blob_centers=True):
 
 
 def rec_movie(traces, fpath, dt, speed_ratio=1/4, cmap='magma'):
-# Paramètres de la vidéo
+    # Paramètres de la vidéo
     fps = int(1/dt) * speed_ratio  # frames per second
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Codec
 
@@ -106,77 +106,158 @@ def plot_4d_projection(traces, basis=None, eigval=None, dpi=100):
 
 
 def plot_cell_connectivity(grid, ax, i=None, title='Example cell connectivity', cbar=True, cmap='viridis'):
+    
     if i is None: 
         i = torch.randint(0, grid.n**2, (1, ))
+        
     im = ax.imshow(grid.W.cpu()[i].reshape(grid.n, grid.n), cmap=cmap)
+    
     ax.scatter(i % grid.n, i // grid.n, marker='x', c='red', label='Focal cell location')
+    
     ax.legend(loc='upper right', frameon=True, fontsize=8)
+    
     ax.set_xticks([])
     ax.set_yticks([])
+    
     ax.set_title(title)
+    
     if cbar:
         cbar = plt.colorbar(im, ax=ax, orientation='vertical', pad=0.02, shrink=0.8)
         cbar.set_label('Weight', rotation=270, labelpad=10)
+        
     return None
 
 def plot_s0(grid, ax, title='Resting state activity', cbar=True, cmap='magma'):
+    
     im = ax.imshow(grid.s0.cpu().reshape(grid.n, grid.n), cmap=cmap)
-    ax.grid(False)
+        
     ax.set_xticks([])
     ax.set_yticks([])
+    
     ax.set_title(title, fontsize=10)
 
     if cbar:
         cbar = plt.colorbar(im, ax=ax, orientation='vertical', pad=0.02, shrink=0.8)
         cbar.set_label('Cell activation', rotation=270, labelpad=10)
+        
     return None
     
 
 
 def network_presentation(grid, show=True):
-    # Create a figure and define the grid specification with adjusted width ratios
+    
     fig = plt.figure(figsize=(12, 3))
     
-    # Define GridSpec with unequal column widths
     spec = gridspec.GridSpec(1, 4, figure=fig, width_ratios=[1, 1, 1, 1.15], wspace=0.01)  # Small space between plots 2, 3, and 4
     
-    # Plot the first figure (plot_s0)
     ax0 = fig.add_subplot(spec[0, 0])
     plot_s0(grid, ax0, cbar=True)
 
-    # Plot the similar plots (2, 3, and 4)
     for i in range(3):
         ax = fig.add_subplot(spec[0, i + 1])
         plot_cell_connectivity(grid, ax, cbar=(i == 2))
 
-    # Adjust margins: Add extra space between plot 1 and plot 2, but keep others close
     plt.subplots_adjust(left=0.0, right=0.95, bottom=0.1, top=0.9, wspace=0.4)
     
     if show:
         plt.show()
+        
     return fig, [fig.axes[i] for i in range(4)]
 
 
-def plot_trajectory(loc_x, loc_y, box_width=None, box_height=None, ax=None, show=False, plot_kwargs={}):
+def plot_trajectory(loc_x, loc_y, box_width=None, box_height=None, ax=None, show=False, show_limits = True, plot_kwargs={}):
+    
     if ax is None:
         fig, ax = plt.subplots()
+        
     ax.plot(loc_x, loc_y, **plot_kwargs)
+    
     if box_width is not None:
         ax.set_xlim(-box_width/2, box_width/2)
+        
     if box_height is not None:
         ax.set_ylim(-box_height/2, box_height/2)
+
+    if show_limits : 
+        ax.scatter(loc_x[0], loc_y[0], c='blue', s=10, marker='o', label='Initial position')
+        ax.scatter(loc_x[-1], loc_y[-1], c='green', s=10, marker='o', label='Final position')
+        
     if show :
         plt.show()
+        
     return ax
 
 
 def compare_model_prediction(predicted_position, true_position, box_width=None, box_height=None, show=True):
+    
     fig, ax = plt.subplots(figsize=(5, 5), dpi = 200)
+    
     kw = {'c': 'black', 'ls': '-', 'lw':1.4, 'label':'True position'}
-    ax = plot_trajectory(*true_position.T, box_width = box_width, box_height = box_height,ax=ax, show=False, plot_kwargs=kw)
+    ax = plot_trajectory(*true_position.T, box_width = box_width, box_height = box_height,ax=ax, show=False, show_limits = True, plot_kwargs=kw)
+    
     kw = {'c': 'red', 'ls': ':', 'lw':0.7, 'label':'Model prediction'}
-    ax = plot_trajectory(*predicted_position.T, box_width = box_width, box_height = box_height,ax=ax, show=False, plot_kwargs=kw)
+    ax = plot_trajectory(*predicted_position.T, box_width = box_width, box_height = box_height,ax=ax, show=False, show_limits = False, plot_kwargs=kw)
+    
     ax.scatter(*true_position[0], c='blue', s=10, marker='o', label='Initial position')
     ax.scatter(*true_position[-1], c='green', s=10, marker='o', label='Final position')
+    
     plt.legend()
-    plt.show()
+    if show :
+        plt.show()
+
+    return ax
+
+
+def place_cells_activity(place_cells, idx = None, ax=None, show=True, cbar = True, cmap = 'magma'):
+    if idx is None :
+        idx = np.random.randint(0, place_cells.sheet_size**2)
+
+    loc = place_cells.loc.unsqueeze(1).to(place_cells.device)
+
+    activity = place_cells.out(loc)[idx].cpu().reshape(place_cells.sheet_size, place_cells.sheet_size)
+
+    if ax is None : 
+        fig, ax = plt.subplots()
+
+    im= ax.imshow(activity, cmap = cmap)
+    
+    if cbar:
+        cbar = plt.colorbar(im, ax=ax, orientation='vertical', pad=0.02, shrink=0.8)
+        cbar.set_label('Cell activation', rotation=270, labelpad=10)
+
+
+    ax.set_title('Arbitrary place cell activation')
+    ax.set_xlabel('Animal x location (m)')
+    ax.set_ylabel('Animal y location (m)')
+    
+    if show:
+        plt.show()
+        
+    return ax
+
+
+def present_training_environment(network, trainer, num_trajectories = 10, show=True):
+    
+    fig, ax = plt.subplots(1, 2, figsize = (10, 5))
+    
+    place_cells_activity(network.place_cells, ax=ax[0], show=False)
+    
+    trajectories = next(trainer.generator.generator())[1].cpu()
+    
+    for k in range(num_trajectories):
+        kw = {'c': 'black', 'ls': '-', 'lw':1.4}
+        plot_trajectory(*trajectories[:, k,:].T, ax=ax[1], box_width = network.options.box_width, box_height = network.options.box_width, plot_kwargs = kw)
+
+    handles, labels = ax[1].get_legend_handles_labels()
+    unique = dict(zip(labels, handles))
+    ax[1].legend(unique.values(), unique.keys())
+    
+    ax[1].set_aspect('equal')
+    ax[1].set_title('Example training trajectories')
+
+    plt.tight_layout()
+    
+    if show:
+        plt.show()
+    
+    return fig, ax
